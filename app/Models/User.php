b2600ea13ee375d9998f688240210fba1faeffc1
okumura-e <adrielitamar@gmail.com>
2022-09-29 16:39:04 -0300

    add model and migrations

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
+    
+    public function notices()
+    {
+        return $this->hasMany('App\Models\Notice');
+    }
 }
